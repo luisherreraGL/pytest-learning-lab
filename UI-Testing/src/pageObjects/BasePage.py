fec3c18be65b9ebb853f302e775effb1d179afd2
luisherreraGL <luis.herrera@gorillalogic.com>
2022-06-24 17:25:12 -0600

    Adding allure

--- UI-Testing/src/pageObjects/BasePage.py
+++ UI-Testing/src/pageObjects/BasePage.py
@@ -1,4 +1,5 @@
 from src.pageObjects.Base import Base
+from allure import step
 
 class BasePage (Base):
     path = ""
@@ -7,6 +8,7 @@ class BasePage (Base):
     def __init__(self, browserActions) -> None:
        self.browserActions =  browserActions
 
+    @step("Load the page")
     def open(self):
         self.browserActions.navigateWithBaseurl(self.path)
         #quitar baseurl
--- UI-Testing/src/pageObjects/Navbar.py
+++ UI-Testing/src/pageObjects/Navbar.py
@@ -1,10 +1,12 @@
 from src.locators.NavbarLocators import NavbarLocators
 from src.pageObjects.Base import Base
+from allure import step
 class Navbar (Base):
     locators = NavbarLocators()
 
     def __init__(self, browserActions) -> None:
         super().__init__(browserActions)
 
+    @step("Click on Contact Link")
     def clickOnContact(self):
         self.browserActions.click(self.locators.contactUsingLink)
