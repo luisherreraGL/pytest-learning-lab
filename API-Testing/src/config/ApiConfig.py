ab36264333f7c6c90788f502acdc159479a5fc55
luisherreraGL <luis.herrera@gorillalogic.com>
2022-07-05 14:50:52 -0600

    Add defaultHeaders

---
+++ API-Testing/src/config/ApiConfig.py
@@ -0,0 +1,5 @@
+import os
+
+credentials = {
+    "api_key": os.getenv('API_KEY') or ""
+}
--- API-Testing/test/pet/BasePet.py
+++ API-Testing/test/pet/BasePet.py
@@ -1,6 +1,11 @@
 from src.utils.RestApiClient import RestApiClient
+from src.config.ApiConfig import credentials
 from src.config.EnvSettings import EnvSettings
-
+import pytest
 class BasePet():
     client =  RestApiClient(EnvSettings().getEnvConfig()["baseurl"])
     path = 'v2/pet/'
+
+    @pytest.fixture(autouse=True)
+    def setAuth(self):
+        self.client.addDefaultHeaders({"api_key": credentials["api_key"]})
